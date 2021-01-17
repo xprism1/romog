382727fbc616c808711554df3fda8a989bf4e83c
xprism1 <xprism01@gmail.com>
2021-01-17 23:17:22 +0800

    Add header skipping support

--- include/gethashes.h
+++ include/gethashes.h
@@ -3,6 +3,6 @@
 #ifndef GETHASHES_H
 #define GETHASHES_H
 
-std::vector<std::string> getHashes(std::string path);
+std::vector<std::string> getHashes(std::string path, int start_offset = -1, std::vector<std::tuple<int, std::string>> data = {});
 
 #endif
